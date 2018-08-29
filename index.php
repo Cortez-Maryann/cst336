b7986b09073efa28e3962cdccb6268effcbfffd3
Maryann Cortez <marcortez@csumb.edu>
2018-08-29 22:05:01 +0000

    Adding Lab 1 files.

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<html><body>
+<html>
+    <head>
+        <title>Maryann</title>
+    </head><body>
 <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
