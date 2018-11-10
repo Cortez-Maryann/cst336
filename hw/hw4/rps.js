fbf220303bb9d5f0f6d82fc0654d3b908294cf7b
Maryann Cortez <marcortez@csumb.edu>
2018-11-10 01:29:30 +0000

    Updating Hw 4

--- hw/hw4/rps.js
+++ hw/hw4/rps.js
@@ -6,7 +6,7 @@ var btnGo;
 var computerChoice;
 var playerChoice;
 
-function init(){
+function init() {
 	imgPlayer = document.getElementById("imgPlayer");
 	btnRock = document.getElementById("btnRock");
 	btnPaper = document.getElementById("btnPaper");
@@ -15,105 +15,110 @@ function init(){
 	deselectAll();
 }
 
-function deselectAll(){
+function deselectAll() {
 	btnRock.style.backgroundColor = 'silver';
 	btnPaper.style.backgroundColor = 'silver';
 	btnScissors.style.backgroundColor = 'silver';
-	
 }
 
-function startGame(){
+function startGame() {
 	document.getElementById('introScreen').style.display = 'none';
 }
 
-function replay(){
+function replay() {
 	document.getElementById('endScreen').style.display = 'none';
 	btnGo.style.display = 'none';
 	deselectAll();
 	document.getElementById('lblRock').style.backgroundColor = '#EEEEEE';
 	document.getElementById('lblPaper').style.backgroundColor = '#EEEEEE';
 	document.getElementById('lblScissors').style.backgroundColor = '#EEEEEE';
-	
 	imgPlayer.src = 'images/question.png';
 	document.getElementById('imgComputer').src = 'images/question.png';
-	
 }
 
-function select(choice){
+function select(choice) {
 	playerChoice = choice;
 	imgPlayer.src = 'images/' + choice + '.png';
 	deselectAll();
-	if(choice =='rock') btnRock.style.backgroundColor = '#888888';
-	if(choice =='paper') btnPaper.style.backgroundColor = '#888888';
-	if(choice =='scissors') btnScissors.style.backgroundColor = '#888888';
+	if (choice == 'rock') btnRock.style.backgroundColor = '#888888';
+	if (choice == 'paper') btnPaper.style.backgroundColor = '#888888';
+	if (choice == 'scissors') btnScissors.style.backgroundColor = '#888888';
 	btnGo.style.display = 'block';
 }
 
-function go(){
+function go() {
 	var txtEndMessage = document.getElementById('txtEndMessage');
 	var txtEndTitle = document.getElementById('txtEndTitle');
-	
-	var numChoice = Math.floor(Math.random()*3);
+
+	var numChoice = Math.floor(Math.random() * 3);
 	var imgComputer = document.getElementById('imgComputer');
-	
+
 	document.getElementById('lblRock').style.backgroundColor = '#EEEEEE';
 	document.getElementById('lblPaper').style.backgroundColor = '#EEEEEE';
 	document.getElementById('lblScissors').style.backgroundColor = '#EEEEEE';
-	
-	if(numChoice == 0){
-		computerChoice = 'rock';
-		imgComputer.src = 'images/rock.png';
-		document.getElementById('lblRock').style.backgroundColor = 'yellow';
-		if(playerChoice = 'rock'){
-			txtEndTitle.innerHTML = '';
-			txtEndMessage.innerHTML = 'Tie';
-		}
-		else if(playerChoice = 'paper') {
-			txtEndTitle.innerHTML = '';
-			txtEndMessage.innerHTML = 'You Win';
-		}
-		else if(playerChoice = 'scissors'){
-			txtEndTitle.innerHTML = '';
-			txtEndMessage.innerHTML = 'You Lose';
-		}
-	} 
-	else if(numChoice == 1){ 
-		computerChoice = 'paper';
-		imgComputer.src = 'images/paper.png';
-		document.getElementById('lblPaper').style.backgroundColor = 'yellow';
-		if(playerChoice = 'rock'){
-			txtEndTitle.innerHTML = '';
-			txtEndMessage.innerHTML = 'You Lose';
-		}
-		else if(playerChoice = 'paper'){
-			txtEndTitle.innerHTML = '';
-			txtEndMessage.innerHTML = 'Tie';
-		}
-		else if(playerChoice = 'scissors') {
-			txtEndTitle.innerHTML = '';
-			txtEndMessage.innerHTML = 'You Win';
-		}
+
+	switch (numChoice) {
+		case 0:
+			computerChoice = 'rock';
+			imgComputer.src = 'images/rock.png';
+			document.getElementById('lblRock').style.backgroundColor = 'yellow';
+			if (playerChoice = 'rock') {
+				txtEndTitle.innerHTML = '';
+				txtEndMessage.innerHTML = 'Tie';
+			}
+			else if (playerChoice = 'paper') {
+				txtEndTitle.innerHTML = '';
+				txtEndMessage.innerHTML = 'You Win';
+			}
+			else if (playerChoice = 'scissors') {
+				txtEndTitle.innerHTML = '';
+				txtEndMessage.innerHTML = 'You Lose';
+			}
+			break;
+
+		case 1:
+			computerChoice = 'paper';
+			imgComputer.src = 'images/paper.png';
+			document.getElementById('lblPaper').style.backgroundColor = 'yellow';
+			if (playerChoice = 'rock') {
+				txtEndTitle.innerHTML = '';
+				txtEndMessage.innerHTML = 'You Lose';
+			}
+			else if (playerChoice = 'paper') {
+				txtEndTitle.innerHTML = '';
+				txtEndMessage.innerHTML = 'Tie';
+			}
+			else if (playerChoice = 'scissors') {
+				txtEndTitle.innerHTML = '';
+				txtEndMessage.innerHTML = 'You Win';
+			}
+			break;
+		
+		case 2:
+			computerChoice = 'scissors';
+			imgComputer.src = 'images/scissors.png';
+			document.getElementById('lblScissors').style.backgroundColor = 'yellow';
+
+			if (playerChoice = 'rock') {
+				txtEndTitle.innerHTML = '';
+				txtEndMessage.innerHTML = 'You Win';
+			}
+
+			else if (playerChoice = 'paper') {
+				txtEndTitle.innerHTML = '';
+				txtEndMessage.innerHTML = 'You Lose';
+			}
+
+			else if (playerChoice = 'scissors') {
+				txtEndTitle.innerHTML = '';
+				txtEndMessage.innerHTML = 'Tie';
+			}
+			break;
+		default:
+			console.log('This shouldn\'t happen');
+			break;			
 	}
-	else if(numChoice == 2){
-		computerChoice = 'scissors';
-		imgComputer.src = 'images/scissors.png';
-		document.getElementById('lblScissors').style.backgroundColor = 'yellow';
-		if(playerChoice = 'rock'){
-			txtEndTitle.innerHTML = '';
-			txtEndMessage.innerHTML = 'You Win';
-		}
-		else if(playerChoice = 'paper'){
-			txtEndTitle.innerHTML = '';
-			txtEndMessage.innerHTML = 'You Lose';
-		}
-		else if(playerChoice = 'scissors'){
-			txtEndTitle.innerHTML = '';
-			txtEndMessage.innerHTML = 'Tie';
-		}
-	}
-	
+
 	document.getElementById('endScreen').style.display = 'block';
-	
-	
 }
 
