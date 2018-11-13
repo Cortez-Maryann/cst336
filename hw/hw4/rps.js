7ae3427eb6a405e2b80b6b4e19d7494e5b15d059
Maryann Cortez <marcortez@csumb.edu>
2018-11-13 00:51:29 +0000

    Homework 4 upload

--- hw/hw4/rps.js
+++ hw/hw4/rps.js
@@ -1,23 +1,26 @@
+
 var imgPlayer;
-var playerButtonRock;
-var playerButtonPaper;
-var playerButtonScissors;
+var playerButtonRein;
+var playerButtonJunkrat;
+var playerButtonWidowmaker;
 var buttonGo;
 var computerChoice;
 var playerChoice;
+
 
 const COLORS = {
 	lightGray: '#EEEEEE',
 	darkGray: '#888888',
 	yellow: '#FFFF00',
-	silver: '#C0C0C0'
+	silver: '#C0C0C0',
+	green: '19f428'
 }
 
 function init() {
 	imgPlayer = document.getElementById("imgPlayer");
-	playerButtonRock = document.getElementById("btnRock");
-	playerButtonPaper = document.getElementById("btnPaper");
-	playerButtonScissors = document.getElementById("btnScissors");
+	playerButtonRein = document.getElementById("btnRock");
+	playerButtonJunkrat = document.getElementById("btnPaper");
+	playerButtonWidowmaker = document.getElementById("btnScissors");
 	buttonGo = document.getElementById("btnGo");
 	deselectAll();
 }
@@ -26,9 +29,9 @@ function init() {
  * Deselects the previous chosen move.
  */
 function deselectAll() {
-	playerButtonRock.style.backgroundColor = COLORS.silver;
-	playerButtonPaper.style.backgroundColor = COLORS.silver;
-	playerButtonScissors.style.backgroundColor = COLORS.silver;
+	playerButtonRein.style.backgroundColor = COLORS.silver;
+	playerButtonJunkrat.style.backgroundColor = COLORS.silver;
+	playerButtonWidowmaker.style.backgroundColor = COLORS.silver;
 }
 
 function setBackgroundColor(cssClass, color) {
@@ -39,173 +42,65 @@ function setBackgroundColor(cssClass, color) {
  * Closes the introduction screen to display the game screen.
  */
 function startGame() {
-	document.getElementById('introScreen').style.display = 'none';
-}
-
-/**
- * Resets the finished game.
- */
-function replay() {
-	document.getElementById('endScreen').style.display = 'none';
-	buttonGo.style.display = 'none';
-	deselectAll();
-	setBackgroundColor('lblRock', COLORS.lightGray);
-	setBackgroundColor('lblPaper', COLORS.lightGray);
-	setBackgroundColor('lblScissors', COLORS.lightGray);
-	imgPlayer.src = 'images/question.png';
-	document.getElementById('imgComputer').src = 'images/question.png';
-}
-
-
-function select(choice) {
-	playerChoice = choice;
-	imgPlayer.src = `images/${choice}.png`;
-	deselectAll();
-	if (choice == 0) playerButtonRock.style.backgroundColor = COLORS.darkGray;
-	if (choice == 1) playerButtonPaper.style.backgroundColor = COLORS.darkGray;
-	if (choice == 2) playerButtonScissors.style.backgroundColor = COLORS.darkGray;
-	buttonGo.style.display = 'block';
-}
-
-function setResult(title, message) {
-	txtEndTitle.innerHTML = title;
-	txtEndMessage.innerHTML = message;
-}
-
-function gameResult(computerResult, playerResult) {
-	let resultDifference = Math.abs(computerResult - playerResult);
-
-	if (computerResult === playerResult) {
-		return setResult('', 'Tie');
-	}
-
-	if (resultDifference === 1) {
-		if (computerResult > playerResult) {
-			return setResult('', 'You Lose');
-		}
-		return setResult('', 'You Win');
-	}
-
-	if (resultDifference === 2) {
-		if(computerResult < playerResult){
-			return setResult('', 'You Lose');
-		}
-		return setResult('', 'You Win');
-	}
-}
-
-function start() {
-	var txtEndMessage = document.getElementById('txtEndMessage');
-	var txtEndTitle = document.getElementById('txtEndTitle');
-
-	var numChoice = Math.floor(Math.random() * 3);
-	var imgComputer = document.getElementById('imgComputer');
-
-	setBackgroundColor('lblRock', COLORS.lightGray);
-	setBackgroundColor('lblPaper', COLORS.lightGray);
-	setBackgroundColor('lblScissors', COLORS.lightGray);
-
-	switch (numChoice) {
-		case 0:
-			computerChoice = 'rock';
-			imgComputer.src = 'images/rock.png';
-			setBackgroundColor('lblRock', COLORS.yellow);
-			break;
-
-		case 1:
-			computerChoice = 'paper';
-			imgComputer.src = 'images/paper.png';
-			setBackgroundColor('lblPaper', COLORS.yellow);
-			break;
-		
-		case 2:
-			computerChoice = 'scissors';
-			imgComputer.src = 'images/scissors.png';
-			setBackgroundColor('lblScissors', COLORS.yellow);
-			break;
-		default:
-			console.log('This shouldn\'t happen');
-			break;			
-	}
+	document.getElementById("introScreen").style.display = 'none';
+	document.getElementById("howToPlay").style.display = 'none';
 	
-	gameResult(computerChoice, playerChoice);
-	document.getElementById('endScreen').style.display = 'block';
 }
 
-var imgPlayer;
-var playerButtonRock;
-var playerButtonPaper;
-var playerButtonScissors;
-var buttonGo;
-var computerChoice;
-var playerChoice;
-
-const COLORS = {
-	lightGray: '#EEEEEE',
-	darkGray: '#888888',
-	yellow: '#FFFF00',
-	silver: '#C0C0C0'
-}
-
-function init() {
-	imgPlayer = document.getElementById("imgPlayer");
-	playerButtonRock = document.getElementById("btnRock");
-	playerButtonPaper = document.getElementById("btnPaper");
-	playerButtonScissors = document.getElementById("btnScissors");
-	buttonGo = document.getElementById("btnGo");
-	deselectAll();
-}
-
-/**
- * Deselects the previous chosen move.
- */
-function deselectAll() {
-	playerButtonRock.style.backgroundColor = COLORS.silver;
-	playerButtonPaper.style.backgroundColor = COLORS.silver;
-	playerButtonScissors.style.backgroundColor = COLORS.silver;
-}
-
-function setBackgroundColor(cssClass, color) {
-	document.getElementById(cssClass).style.backgroundColor = color;
-}
-
-/**
- * Closes the introduction screen to display the game screen.
- */
-function startGame() {
-	document.getElementById('introScreen').style.display = 'none';
+function help(){
+	document.getElementById("howToPlay").style.display = 'block';
 }
 
 /**
  * Resets the finished game.
  */
 function replay() {
-	document.getElementById('endScreen').style.display = 'none';
+	document.getElementById("endScreen").style.display = 'none';
 	buttonGo.style.display = 'none';
 	deselectAll();
-	setBackgroundColor('lblRock', COLORS.lightGray);
-	setBackgroundColor('lblPaper', COLORS.lightGray);
-	setBackgroundColor('lblScissors', COLORS.lightGray);
-	imgPlayer.src = 'images/question.png';
-	document.getElementById('imgComputer').src = 'images/question.png';
+	setBackgroundColor('lblRein', COLORS.lightGray);
+	setBackgroundColor('lblJunk', COLORS.lightGray);
+	setBackgroundColor('lblWidow', COLORS.lightGray);
+	imgPlayer.src = 'images/select.png';
+	document.getElementById("imgComputer").src = 'images/select.png';
 }
 
-
+/**
+ * Highlights selected character.
+ */
 function select(choice) {
 	playerChoice = choice;
-	imgPlayer.src = 'images/' + choice + '.png';
 	deselectAll();
-	if (choice == 0) playerButtonRock.style.backgroundColor = COLORS.darkGray;
-	if (choice == 1) playerButtonPaper.style.backgroundColor = COLORS.darkGray;
-	if (choice == 2) playerButtonScissors.style.backgroundColor = COLORS.darkGray;
+	if (choice == 0) {
+		playerButtonRein.style.backgroundColor = COLORS.darkGray;
+		imgPlayer.src = 'images/reinhardt.png';
+	}
+	if (choice == 1) {
+		playerButtonJunkrat.style.backgroundColor = COLORS.darkGray;
+		imgPlayer.src = 'images/junkrat.png';
+	}
+	if (choice == 2) {
+		playerButtonWidowmaker.style.backgroundColor = COLORS.darkGray;
+		imgPlayer.src = 'images/widowmaker.png';
+	}
 	buttonGo.style.display = 'block';
 }
 
+
+/**
+ * Displays the result from the game.
+ */
 function setResult(title, message) {
-	txtEndTitle.innerHTML = title;
-	txtEndMessage.innerHTML = message;
+	document.getElementById("txtEndTitle").innerHTML = title;
+	document.getElementById("txtEndMessage").innerHTML = message;
+	document.getElementById("txtEndMessage").style.color = COLORS.green;
+
 }
 
+
+/**
+ * Calculates the win condition and displays it.
+ */
 function gameResult(computerResult, playerResult) {
 	var resultDifference = Math.abs(computerResult - playerResult);
 
@@ -213,7 +108,7 @@ function gameResult(computerResult, playerResult) {
 		setResult('', 'Tie');
 	}
 
-	if (resultDifference === 1) {
+	else if (resultDifference === 1) {
 		if (computerResult > playerResult) {
 			setResult('', 'You Lose');
 			return;
@@ -221,7 +116,7 @@ function gameResult(computerResult, playerResult) {
 		setResult('', 'You Win');
 	}
 
-	if (resultDifference === 2) {
+	else if (resultDifference === 2) {
 		if(computerResult < playerResult){
 			setResult('', 'You Lose');
 			return;
@@ -230,34 +125,35 @@ function gameResult(computerResult, playerResult) {
 	}
 }
 
-function start() {
-	var txtEndMessage = document.getElementById('txtEndMessage');
-	var txtEndTitle = document.getElementById('txtEndTitle');
 
+/**
+ * This function gives the computer a random value and displays the game result.
+ */
+function start() {
 	var numChoice = Math.floor(Math.random() * 3);
 	var imgComputer = document.getElementById('imgComputer');
 
-	setBackgroundColor('lblRock', COLORS.lightGray);
-	setBackgroundColor('lblPaper', COLORS.lightGray);
-	setBackgroundColor('lblScissors', COLORS.lightGray);
+	setBackgroundColor('lblRein', COLORS.lightGray);
+	setBackgroundColor('lblJunk', COLORS.lightGray);
+	setBackgroundColor('lblWidow', COLORS.lightGray);
 
 	switch (numChoice) {
 		case 0:
-			computerChoice = 'rock';
-			imgComputer.src = 'images/rock.png';
-			setBackgroundColor('lblRock', COLORS.yellow);
+			computerChoice = 0;
+			imgComputer.src = 'images/reinhardt.png';
+			setBackgroundColor('lblRein', COLORS.yellow);
 			break;
 
 		case 1:
-			computerChoice = 'paper';
-			imgComputer.src = 'images/paper.png';
-			setBackgroundColor('lblPaper', COLORS.yellow);
+			computerChoice = 1;
+			imgComputer.src = 'images/junkrat.png';
+			setBackgroundColor('lblJunk', COLORS.yellow);
 			break;
 		
 		case 2:
-			computerChoice = 'scissors';
-			imgComputer.src = 'images/scissors.png';
-			setBackgroundColor('lblScissors', COLORS.yellow);
+			computerChoice = 2;
+			imgComputer.src = 'images/widowmaker.png';
+			setBackgroundColor('lblWidow', COLORS.yellow);
 			break;
 		default:
 			console.log('This shouldn\'t happen');
